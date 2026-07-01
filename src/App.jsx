fcad6187434a3ec6c5f4e6105672d7c9c8f63718
Damodar <damodardosa11@outlook.com>
2026-07-01 17:00:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1 className='text-2xl font-bold text-center'>Welcome to my 3D Portfolio</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
